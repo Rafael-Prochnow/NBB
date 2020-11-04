29305f72047dbc3bc54564627b784430f417e23a
Rafael-Prochnow <rafaprochnow31@gmail.com>
2020-11-04 12:14:42 -0300

    p10

--- te.py
+++ te.py
@@ -62,54 +62,37 @@ soup = BeautifulSoup(html_content, 'html.parser')
 acoes = soup.get_text()
 
 acoes = soup.get_text()
-b = acoes.replace(' 1º', '\n1')
-b = b.replace(' 2º', '\n2')
-b = b.replace(' 3º', '\n3')
-b = b.replace(' 4º', '\n4')
-b = re.sub('(.			 |      |  |    |			 |			 |			)', ';', b)
 
-data = io.StringIO(b)
+a = acoes.replace(' 1º', '\n1')
+a = a.replace(' 2º', '\n2')
+a = a.replace(' 3º', '\n3')
+a = a.replace(' 4º', '\n4')
+a = re.sub('(.			 |			 |			 )', ';', a)
+a = a.replace('    ', ';')
+a = a.replace('  ', ';')
+a = a.replace(';;', ';')
 
-df = pd.read_csv(data, sep=';', index_col=False,
-                         usecols=[0, 1, 2, 3, 4, 5], header=None)
+a = re.sub("(do quarto quarto|INÍCIO DE QUARTO Início do terceiro quarto|"
+           "INÍCIO DE QUARTO Início do segundo quarto)", "", a)
 
-df.columns = ['Quarto', 'Tempo', 'Placar', 'Time', 'Inf_1', 'Inf_2']
-
-divisao1_placar = df["Placar"].str.split(" x ")
-
-placar_casa = divisao1_placar.str.get(0)
-placar_visitante = divisao1_placar.str.get(1)
-df['placar_casa'] = placar_casa
-df['placar_visitante'] = placar_visitante
-df.drop('Placar', axis=1, inplace=True)
-
-dados = df.assign(inf_3=df.Inf_1.astype(str) + ' ' + df.Inf_2.astype(str))
-dados.drop('Inf_1', axis=1, inplace=True)
-dados.drop('Inf_2', axis=1, inplace=True)
-divisao1 = dados["inf_3"]
-
-divisao1_retirado = divisao1.str.translate({ord(c): " " for c in "!_+"})
-
-a = re.sub('(INÍCIO DE QUARTO Início do quarto quarto|INÍCIO DE QUARTO Início do terceiro quarto|'
-       'INÍCIO DE QUARTO Início do segundo quarto)', 'inicio_quarto;', str(divisao1_retirado))
-
-
-'''
-a = a.replace('FIM DE PARTIDA Fim de partida', 'fim_partida;')
-a = a.replace('INÍCIO DE QUARTO Início de partida', 'inicio_partida;')
+a = a.replace("INÍCIO DE QUARTO Início do", ";inicio_quarto")
+a = a.replace("FIM DE QUARTO Fim", ";fim_quarto")
 
 a = re.sub('(FIM DE QUARTO Fim do quarto quarto|FIM DE QUARTO Fim do terceiro quarto|'
-            'FIM DE QUARTO Fim do segundo quarto|FIM DE QUARTO Fim do primeiro quarto)', 'fim_quarto;', a)
-# três pontos
+            'FIM DE QUARTO Fim do segundo quarto|FIM DE QUARTO Fim do primeiro quarto)', '', a)
+a = a.replace('FIM DE PARTIDA Fim de partida', ';fim_partida;')
+a = a.replace('INÍCIO DE QUARTO Início de partida', ';inicio_partida;')
 
+
+# três pontos
 a = a.replace("Tentativa para três pontos ", "3_Pts_T;")
-a = a.replace('É DE TRÊS  É de três  ', '3_Pts_C;')
+a = a.replace('É DE TRÊS! É de três! ', '3_Pts_C;')
 # lance livre
-a = a.replace(' 1 PONTO ', 'LL_Pts_C;')
+a = a.replace('+1 PONTO ', 'LL_Pts_C;')
 a = a.replace('Lance Livre Errado ', 'LL_Pts_T;')
 # dois pontos
 a = a.replace('Tentativa para dois pontos ', '2_Pts_T;')
-a = a.replace(' 2 PONTOS ', '2_Pts_C;')
+a = a.replace('+2 PONTOS ', '2_Pts_C;')
 # rebotes
 a = a.replace('REBOTE DEFENSIVO ', 'RD;')
 a = a.replace('REBOTE OFENSIVO ', 'RO;')
@@ -130,39 +113,33 @@ a = a.replace('Substituição Sai ', 'substituicao_sai;')
 # tocos
 a = a.replace('TOCO  ', 'TO;')
 # tempo técnico
-a = a.replace(' TEMPO TÉCNICO Técnico da equipe ', 'tempo_tecnico;')
+a = a.replace('TEMPO TÉCNICO Técnico da equipe ', 'tempo_tecnico;')
 # Erros
 a = re.sub('( Violação Estouro dos 24s|Violação |Erro )', 'ER;', a)
 # enterrada
-a = a.replace('CRAVADA  Cravada ', 'EN;')
+a = a.replace('CRAVADA! Cravada!', 'EN;')
 # retiradas de informações inúteis
 a = re.sub('( erra tentativa para três pontos| acerta arremesso de três pontos| acerta o lance livre|'
            ' erra tentativa para dois pontos| acerta arremesso de dois pontos| pega rebote ofensivo|'
            ' erra o lance livre| pega rebote defensivo| recupera a bola| sofre falta| comete falta ofensiva|'
            ' antidesportiva| perde posse de bola| comete violação de saída de quadra|nan| dá um toco|'
-           ' pede tempo| acerta enterrada | comete falta técnica| comete falta)', '', a)
-
-'''
-dados['mudados'] = a
-dados.drop('inf_3', axis=1, inplace=True)
-
-'''
-separar_01 = dados['mudados'].str.split(";")
-Indicador = separar_01.str.get(0)
-dados['Indicador'] = Indicador
-inf_02 = separar_01.str.get(1)
+           ' pede tempo| acerta enterrada | comete falta técnica| comete falta|Estouro dos 24s|'
+           ' acerta enterrada)', '', a)
 
 
-teste = inf_02.str.translate({ord(c): "," for c in "("})
+data = io.StringIO(a)
 
-teste2 = teste.str.split(',')
-teste3 = teste2.str.get(0)
-dados['Nomes'] = teste3
-dados.drop('mudados', axis=1, inplace=True)
+df = pd.read_csv(data, sep=';', index_col=False,
+                 usecols=[0, 1, 2, 3, 4, 5], header=None)
+df.columns = ['Quarto', 'Tempo', 'Placar', 'Time', 'Indicador', 'Nome']
 
-'''
-dados.to_csv('parte_3.csv')
-# fora revisados os dados na primeira planilha. eu sei que está certo uma olhada pq
-# os dados apresentam padrão na escrita
+divisao1_placar = df["Placar"].str.split(" x ")
+placar_casa = divisao1_placar.str.get(0)
+placar_visitante = divisao1_placar.str.get(1)
+df['placar_casa'] = placar_casa
+df['placar_visitante'] = placar_visitante
+df.drop('Placar', axis=1, inplace=True)
 
-# dados.drop('inf_3', axis=1, inplace=True)
+df = df[['Quarto', 'Tempo', 'placar_casa', 'placar_visitante', 'Time', 'Indicador', 'Nome']]
+
+df.to_csv('parte_3.csv')
